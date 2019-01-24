ae36c71409a8cbe14e81936c26616dcb806cb9d9
jackson.hou <jackson.hou@vipshop.com>
2019-01-24 13:38:36 +0800

    change to use log4j for compatibility

--- core/src/test/scala/com/cloudera/spark/PeakReportingSuite.scala
+++ core/src/test/scala/com/cloudera/spark/PeakReportingSuite.scala
@@ -1,11 +1,17 @@
 // (c) Copyright 2018 Cloudera, Inc. All rights reserved.
 package com.cloudera.spark
 
+import com.typesafe.scalalogging.Logger
 import org.scalatest.FunSuite
 
 class PeakReportingSuite extends FunSuite {
 
+  private[this] val log = Logger(this.getClass)
+
   test("increment bytes") {
+
+    log.info("================== test IncrementBytes.report start.")
+
     // delta over 1e7, and 5% increase
     assert(IncrementBytes.report(1e9.toLong, 1.051e9.toLong))
     // delta over 1e7, but less than 5% increase
@@ -18,6 +24,8 @@ class PeakReportingSuite extends FunSuite {
 
     // increase from small starting point OK
     assert(IncrementBytes.report(0, 1.001e7.toLong))
+
+    log.info("================== test IncrementBytes.report end.")
 
   }
 }
